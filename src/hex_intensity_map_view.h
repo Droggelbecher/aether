e76d07c7327172cd92a3a889bc4d76fffe9b87cc
Henning Hasemann <henning@automotive-ai.com>
2019-05-22 20:21:10 +0200

    intensity map view code cleanup

--- src/hex_intensity_map_view.h
+++ src/hex_intensity_map_view.h
@@ -48,10 +48,10 @@ private:
 		using xt::strided_view;
 		//using xt::ellipsis;
 
-		double sz = 100;
+		//double sz = 100;
 		auto r = _texture.render_start();
 
-		auto hex = hex_polygon() * 100;
+		auto hex = transform_3d_2d(hex_polygon());
 
 		double h = sqrt(3.) / 2.;
 		auto offsets = xarray<double>{
@@ -60,23 +60,17 @@ private:
 			{ 3. , 0., 0. },
 			//{ 1.5, .5 }
 		};
-		offsets *= sz;
-		offsets += xarray<double>{ 100, 100, 0. };
+		//offsets += xarray<double>{ 100, 100, 0. };
 
 		for(size_t i = 0; i < offsets.shape()[0]; i++) {
-			//auto offset = xt::linalg::dot(view(offsets, i, all()), view_transform());
-			//auto offset = xt::linalg::dot(view(offsets, i, all()), view_transform());
-			auto offset = project(view(offsets, i, all()));
-			fmt::print("offset={0} {1}\n", offset(0), offset(1));
-		
-			auto poly = project(hex) + offset; //xt::linalg::dot(hex, view_transform()) + offset;
-			fmt::print("poly.sh={0} {1}\n", poly.shape()[0], poly.shape()[1]);
+			auto offset = transform_3d_2d(view(offsets, i, all()));
+
+			auto poly = transform_2d_screen(hex + offset);
 			auto xs = xarray<int16_t>(view(poly, all(), 0));
 			auto ys = xarray<int16_t>(view(poly, all(), 1));
 
-			aacircleRGBA(r, offset(0), offset(1), sz, 255, 255, 255, 100);
-
-			aapolygonRGBA(r, xs.data(), ys.data(), xs.size(), 220, 230, 240, 255);
+			filledPolygonRGBA(r, xs.data(), ys.data(), xs.size(), 100, 100, 100, 255);
+			aapolygonRGBA(r, xs.data(), ys.data(), xs.size(), 255, 255, 255, 255);
 		}
 
 		//SDL_SetRenderTarget(r, _sdl_texture);
@@ -98,7 +92,7 @@ private:
 	xt::xarray<double> hex_polygon() {
 		// edge length = 1, center at 0/0
 		// "flat" rotation
-		double h = sqrt(3.) / 2.;
+		static const double h = sqrt(3.) / 2.;
 		return {
 			{ - .5, -h, 0. },
 			{   .5, -h, 0. },
@@ -109,29 +103,27 @@ private:
 		};
 	}
 
-	xt::xarray<double> project(xt::xarray<double> a) {
+	xt::xarray<double> transform_3d_2d(xt::xarray<double> a) {
 		using xt::xarray;
 
 		// http://www.petercollingridge.co.uk/tutorials/svg/isometric-projection/
-		const double phi = 0.25 * M_PI; 
-		const xarray<double> rotate_z = {
+		static const double phi = 0.25 * M_PI; 
+		static const xarray<double> rotate_z = {
 			{ cos(phi), -sin(phi), 0. },
 			{ sin(phi), cos(phi), 0. },
 			{ 0., 0., 1. },
 		};
 
-		const double alpha = atan(sqrt(2.));
-		const xarray<double> rotate_x = {
+		static const double alpha = atan(sqrt(2.));
+		static const xarray<double> rotate_x = {
 			{ 1., 0., 0. },
 			{ 0., cos(alpha), -sin(alpha) },
 			{ 0., sin(alpha), cos(alpha) }
 		};
 
-		//const auto M = xt::linalg::dot(rotate_z, rotate_x);
-		const auto M = xt::linalg::dot(rotate_x, rotate_z);
+		static const auto M = xt::linalg::dot(rotate_x, rotate_z);
 		return xt::transpose(view(
 			xt::linalg::dot(
-				//xt::stack(xt::xtuple(a, xarray<double>{0.})),
 				M,
 				xt::transpose(a)
 			),
@@ -140,16 +132,14 @@ private:
 		));
 	}
 
-	xt::xarray<double> view_transform() {
-		double m = 0.;
-		double n = -.3;
-		return { {1., m}, {n, 1.} };
+	xt::xarray<double> transform_2d_screen(xt::xarray<double> a) {
+		return (a + _offset) * _scale;
 	}
-
 
 	//HexIntensityMap _intensity_map;
 	Texture _texture;
 	double _scale;
+	xt::xarray<double> _offset = { 1., 1. };
 };
 
 
